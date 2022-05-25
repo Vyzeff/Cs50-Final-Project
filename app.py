226225a2bd64d29204356a031724f7a7856f9c2d
Vyzeff <ol.sa.gabriel02@gmail.com>
2022-05-25 14:26:38 -0300

    First set of various changes, General Pseudocode

--- app.py
+++ app.py
@@ -1,10 +1,29 @@
 from flask import Flask, render_template, redirect, session, request
 from flask_session import Session
+
+from sqlalchemy import create_engine
+from sqlalchemy_utils import database_exists, create_database
+
+from datetime import datetime
 from helper import login_verify, error
 
+# Configures the app
 app = Flask(__name__)
+app.config["TEMPLATES_AUTO_RELOAD"] = True
+
+# Makes so data is saved in files
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+
+# Defines flask session
 Session(app)
 
+# Creates engine for SQLAlchemy
+engine = create_engine("sqlite3+pysqlite:///homebook.db", echo=True, future=True)
+# If Database isn't present, create one
+if not database_exists(engine.url):
+    create_database(engine.url)
+ 
 @app.route("/")
 @login_verify
 def index():
@@ -16,31 +35,39 @@ def index():
     # uma checklist vai aparecer, a que tem a flag de "importante"
     return render_template("index.html")
 @app.route("/login", methods=["GET", "POST"])
-def login():
+def login(): 
     # will redirect to login page
     # will take a username and password
     # check if those things are in db
     # either log in or error popup
-    db.execute bla bla bla
+    # Forgets user
+    return error("TODO")
     session.clear()
-    session["user_id"] = rows[0]["id"]
+    
+    #if request.method == "GET":
+    #    return render_template("login.html")
+    
+    
+    #session["user_id"] = rows[0]["id"]
 
-@app.route("/register",)
+@app.route("/register", methods=["GET", "POST"])
 def register():
     # will ask for an unused username
     # will ask for a password ate least 8 digits long, together with 1 number and 1 symbol
     # maybe implement captcha???
+    return error("TODO")
 
 @app.route("/logout")
 def logout():
     session.clear()
-    return redirect("/")
+    return error("TODO")
 
 @app.route("/notebook")
 def notebook():
     # will let the user take notes in separate blocks which can be arranged
     # will show all notes
     # notas vao ter titulo, data e corpo
+    return error("TODO")
 
 @app.route("/clock")
 def clocks():
@@ -48,6 +75,7 @@ def clocks():
     # eh possivel escolher relogios de outras regioes
     # criar alarmes e timers
     # multiplos relogios ao mesmo tempo
+    return error("TODO")
 
 @app.route("/checklist")
 def checklists():
@@ -55,6 +83,7 @@ def checklists():
     # vai ser possivel criar uma ou mais checklists
     # uma delas pode ser dada como "importante" e vai aparecer na index
     # da mesma forma do notebook, elas podem ser arrastadas por ai
+    return error("TODO")
 
 @app.route("/water")
 def waterBottle():
@@ -63,3 +92,4 @@ def waterBottle():
     # botao para tomar agua
     # cicla entre imagens de uma garrafa de agua
     # form com botao para tomar uma medida de agua definida pelo usuario
+    return error("TODO")
