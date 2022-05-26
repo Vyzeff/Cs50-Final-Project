14c767163946f2f1effc464c3a677a2cd5c5c260
Vyzeff <ol.sa.gabriel02@gmail.com>
2022-05-26 11:49:17 -0300

    SQLAlchemy Experiments

--- app.py
+++ app.py
@@ -1,38 +1,91 @@
 from flask import Flask, render_template, redirect, session, request
 from flask_session import Session
 
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, ForeignKey
+from sqlalchemy import create_engine, Table, Column, Integer, String, ForeignKey, MetaData
 from sqlalchemy_utils import database_exists, create_database
+from sqlalchemy.orm import declarative_base, sessionmaker
 
 from datetime import datetime
 from helper import login_verify, error
 
-# Configures the app
+
+# Flask
+## Configures the app
 app = Flask(__name__)
 app.config["TEMPLATES_AUTO_RELOAD"] = True
 
-# Makes so data is saved in files
+## Makes so data is saved in files
 app.config["SESSION_PERMANENT"] = False
 app.config["SESSION_TYPE"] = "filesystem"
 
-# Defines flask session
+## Defines flask session
 Session(app)
 
-#Defines metadata from sqlalchemy
-metaObj = MetaData()
-# Creates engine for SQLAlchemy
+
+#SQLAlchemy
+## Orm SQLAlchemy stuff
+metadata_obj = MetaData()
+base = declarative_base()
+
+## Creates engine for SQLAlchemy
 engine = create_engine("sqlite+pysqlite:///homebook.db", echo=True, future=True)
-# If Database isn't present, create one
+
+## If Database isn't present, create one
 if not database_exists(engine.url):
     create_database(engine.url)
+
+
+## Define classes do sqlalchemy
+class Users(base):
+    __tablename__ = "users"
     
-#    user_table = Table(
-#    "user_account",
-#    metaObj,
-#   Column('id', Integer, primary_key=True),
-#    Column('name', String(30)),
-#    Column('fullname', String) )
- 
+    id = Column(Integer, primary_key=True, nullable=False)
+    username = Column(String(20), nullable=False)
+    hash = Column(String, nullable=False)
+    
+    def __repr__(self):
+        return f"User(id={self.id!r}, username={self.username!r}, hash={self.hash!r})"
+class Todo(base):
+    __tablename__ = "todo"
+    
+    id = Column(Integer, primary_key=True, nullable=False)
+    user_id = Column(Integer, ForeignKey('users.id'), nullable=False)
+    todo_text = Column(String, nullable=False)
+    is_complete = Column(Integer, nullable=False)
+    
+    def __repr__(self):
+        return f"Todo(id={self.id!r}, user_id={self.user_id!r}, todo_text={self.todo_text!r}, is_complete={self.is_complete!r})"
+
+# cria essas classes na db se não estiverem ja la.
+base.metadata.create_all(engine)
+
+sqlASession = sessionmaker(bind=engine)
+sqlSession = sqlASession()
+
+
+user = Users(username="teste1", hash="123")
+# insert user na database
+sqlSession.add(user)
+# Ainda não foi salvo na database, mas sim na sessao atual, com o commit é feito a mudança
+sqlSession.commit()
+
+
+# select
+query = sqlSession.query(Users).filter_by(username="teste1").first()
+# <User(username=teste1, hash="123")
+for instance in sqlSession.query(Users).order_by(Users.id):
+    print(instance.username, instance.hash)
+
+#update
+user.username = "teste2"
+sqlSession.commit()
+#delete
+user2 = sqlSession.query(Users).filter_by(username="teste1")
+sqlSession.delete(user2)
+sqlSession.commit
+
+
+
 @app.route("/")
 @login_verify
 def index():
@@ -122,3 +175,21 @@ def waterBottle():
     # cicla entre imagens de uma garrafa de agua
     # form com botao para tomar uma medida de agua definida pelo usuario
     return error("TODO")
+
+
+'''
+TABLES
+CREATE TABLE users(
+  id INT PRIMARY KEY NOT NULL,
+  username STRING NOT NULL,
+  hash STRING NOT NULL  
+);
+CREATE TABLE todo(
+  id INT PRIMARY KEY NOT NULL,
+  user_id INT NOT NULL,
+  todo_text STRING NOT NULL,
+  is_complete INT NOT NULL,
+  FOREIGN KEY(user_id) REFERENCES users (id)
+);
+
+'''
